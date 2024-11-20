d5305bd61df5e251aae7e0344ac035783ed852d5
Matej Bittner <mates-bittner@seznam.cz>
2024-11-20 12:39:19 +0100

    first commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,89 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'Alice Johnson',
+      email: 'alice.johnson@example.com',
+      role: 'INTERN',
+    },
+    {
+      id: 2,
+      name: 'Bob Smith',
+      email: 'bob.smith@example.com',
+      role: 'ENGINEER',
+    },
+    {
+      id: 3,
+      name: 'Charlie Davis',
+      email: 'charlie.davis@example.com',
+      role: 'ADMIN',
+    },
+    {
+      id: 4,
+      name: 'Diana Lee',
+      email: 'diana.lee@example.com',
+      role: 'ENGINEER',
+    },
+    {
+      id: 5,
+      name: 'Ethan Brown',
+      email: 'ethan.brown@example.com',
+      role: 'INTERN',
+    },
+  ];
+
+  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role) {
+      const rolesArray = this.users.filter((user) => user.role === role);
+      if (!rolesArray.length) {
+        throw new NotFoundException('Role not found');
+      }
+    }
+
+    return this.users;
+  }
+
+  findOne(id: number) {
+    const user = this.users.find((user) => user.id === id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+  create(createUserDto: CreateUserDto) {
+    const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id);
+
+    const newUser = {
+      id: usersByHighestId[0].id + 1,
+      ...createUserDto,
+    };
+
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return { ...user, ...updateUserDto };
+      }
+      return user;
+    });
+
+    return this.findOne(id);
+  }
+
+  delete(id: number) {
+    const removedUser = this.findOne(id);
+
+    this.users = this.users.filter((user) => user.id !== id);
+
+    return removedUser;
+  }
+}
